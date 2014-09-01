c2a850722c8689c19811945ecfd6ff7ccb19e45c
JoeLoser <joeloser93@gmail.com>
2014-08-31 21:09:29 -0500

    Reverse string in place in C

---
+++ Arrays and Strings/reverseString.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/*
+	Reverse a string
+ */
+
+void reverse_string(char*);
+
+int main (int argc, char** argv) {
+	printf("The original string is %s\n", argv[1]);
+	reverse_string(argv[1]);
+	printf("The reversed string is %s\n", argv[1]);
+	return 0;
+}
+
+void reverse_string(char* str) {
+	char* end = str;
+	char temp;
+
+	if (str) {
+		// iterate to the end of the string
+		while (*end) {
+			++end;
+		}
+		--end; // go back one char since last char is null terminator
+
+		// swap chars in place starting from far left with the end of the string and then when you meet in the middle, you are done
+		while (str < end) {
+			temp = *str;
+			*str = *end;
+			str++;
+			*end = temp;
+			end--;
+		}
+	}
+}
